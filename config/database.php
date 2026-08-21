37b53fa86e132f2468fe659b76b862f838e4f8d0
John Ermy Arbigoso <johnermyarbigoso@Johns-MacBook-Pro.local>
2026-08-21 22:31:48 +0800

    Update Render entrypoint

--- config/database.php
+++ config/database.php
@@ -86,17 +86,22 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+
+            'url' => env('DB_URL'),
+
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'postgres'),
+            'username' => env('DB_USERNAME', 'postgres'),
             'password' => env('DB_PASSWORD', ''),
+
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+
             'search_path' => env('DB_SCHEMA', 'laravel'),
-            'sslmode' => 'require', // Ensure sslmode is required for Supabase
+
+            'sslmode' => env('DB_SSLMODE', 'require'),
         ],
 
         'sqlsrv' => [
